53693ec6f4675e3158c14af58828745c2e617e16
GeroWalther <gero.walther@gmail.com>
2023-08-27 00:27:24 +0200

    resolver bug

--- services/lib/reducer.js
+++ services/lib/reducer.js
@@ -52,6 +52,10 @@ export function reducer(state, action) {
         ...state,
         converted: convertUnit(action.payload, 'mg', 'lb', 8),
       };
+    case 'µgToGr':
+      return { ...state, converted: (action.payload * 0.000001).toFixed(2) };
+    case 'grToµg':
+      return { ...state, converted: (action.payload * 1000000).toFixed(2) };
     case 'lbToOz':
       return {
         ...state,
@@ -62,6 +66,8 @@ export function reducer(state, action) {
         ...state,
         converted: convertUnit(action.payload, 'oz', 'gr', 2),
       };
+    case 'ozToLb':
+      return { ...state, converted: (action.payload * 0.0625).toFixed(4) };
     case 'grToOz':
       return {
         ...state,
@@ -122,6 +128,10 @@ export function reducer(state, action) {
         ...state,
         converted: convertUnit(action.payload, 'kg', 'µg', 10),
       };
+    case 'lbToµg':
+      return { ...state, converted: action.payload * 453592370 };
+    case 'µgToLb':
+      return { ...state, converted: (action.payload / 453592370).toFixed(10) };
     case 'µgToKg':
       return {
         ...state,
@@ -130,170 +140,426 @@ export function reducer(state, action) {
     case 'cupflToGr':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupfl', 'gr', 2),
+        converted: action.payload * 240,
       };
     case 'cupSugarToGr':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupSugar', 'gr', 2),
+        converted: action.payload * 200,
       };
     case 'cupRiceToGr':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupRice', 'gr', 2),
+        converted: action.payload * 195,
       };
     case 'cupOatsToGr':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupOats', 'gr', 2),
+        converted: action.payload * 90,
       };
     case 'cupFlourToKg':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupFlour', 'kg', 2),
+        converted: action.payload * 0.12,
       };
     case 'cupFlourToMg':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupFlour', 'mg', 2),
+        converted: action.payload * 120000,
       };
     case 'cupFlourToµg':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupFlour', 'µg', 2),
+        converted: action.payload * 120000000,
       };
     case 'cupFlourToTon':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupFlour', 'ton', 2),
+        converted: action.payload * 0.00012,
       };
     case 'cupFlourToLb':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupFlour', 'lb', 2),
+        converted: action.payload * 0.2646,
       };
     case 'cupFlourToOz':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupFlour', 'oz', 2),
+        converted: action.payload * 4,
       };
+
     case 'cupSugarToKg':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupSugar', 'kg', 2),
+        converted: action.payload * 0.2,
       };
     case 'cupSugarToMg':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupSugar', 'mg', 2),
+        converted: action.payload * 200000,
       };
     case 'cupSugarToµg':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupSugar', 'µg', 2),
+        converted: action.payload * 200000000,
       };
     case 'cupSugarToTon':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupSugar', 'ton', 2),
+        converted: action.payload * 0.0002,
       };
     case 'cupSugarToLb':
       return {
         ...state,
-        converted: convertUnit(action.payload, 'cupSugar', 'lb', 2),
+        converted: action.payload * 0.4409,
       };
     case 'cupSugarToOz':
       return {
         ...state,
-        converted: convert(action.payload).from('cup').to('fl-oz').toFixed(2),
+        converted: action.payload * 7.055,
       };
-    case 'mgToCupRice':
+    case 'cupRiceToKg':
       return {
         ...state,
-        converted: convert(action.payload).from('mg').to('cup').toFixed(2),
+        converted: action.payload * 0.18,
       };
-    case 'µgToCupRice':
+    case 'cupRiceToMg':
       return {
         ...state,
-        converted: convert(action.payload).from('µg').to('cup').toFixed(2),
+        converted: action.payload * 180000,
+      };
+    case 'cupRiceToµg':
+      return {
+        ...state,
+        converted: action.payload * 180000000,
+      };
+    case 'cupRiceToTon':
+      return {
+        ...state,
+        converted: action.payload * 0.00018,
+      };
+    case 'cupRiceToLb':
+      return {
+        ...state,
+        converted: action.payload * 0.3968,
+      };
+    case 'cupRiceToOz':
+      return {
+        ...state,
+        converted: action.payload * 6.349,
       };
 
-    case 'tonToCupRice':
+    case 'cupOatsToKg':
       return {
         ...state,
-        converted: convert(action.payload).from('ton').to('cup').toFixed(2),
+        converted: action.payload * 0.11,
+      };
+    case 'cupOatsToMg':
+      return {
+        ...state,
+        converted: action.payload * 110000,
+      };
+    case 'cupOatsToµg':
+      return {
+        ...state,
+        converted: action.payload * 110000000,
+      };
+    case 'cupOatsToTon':
+      return {
+        ...state,
+        converted: action.payload * 0.00011,
+      };
+    case 'cupOatsToLb':
+      return {
+        ...state,
+        converted: action.payload * 0.2425,
+      };
+    case 'cupOatsToOz':
+      return {
+        ...state,
+        converted: action.payload * 3.88,
+      };
+    case 'cupFlourToMg':
+      return {
+        ...state,
+        converted: (action.payload * 240000).toFixed(2),
       };
 
-    case 'lbToCupRice':
+    case 'cupFlourToµg':
       return {
         ...state,
-        converted: convert(action.payload).from('lb').to('cup').toFixed(2),
+        converted: (action.payload * 240000000).toFixed(2),
+      };
+
+    case 'cupFlourToTon':
+      return {
+        ...state,
+        converted: (action.payload * 0.00024).toFixed(2),
+      };
+
+    case 'cupFlourToLb':
+      return {
+        ...state,
+        converted: (action.payload * 0.5283).toFixed(2),
+      };
+
+    case 'cupFlourToOz':
+      return {
+        ...state,
+        converted: (action.payload * 8.4375).toFixed(2),
+      };
+    case 'kgToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload * 8.33333).toFixed(2),
+      };
+
+    case 'lbToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload * 2.26458).toFixed(2),
       };
 
     case 'ozToCupRice':
       return {
         ...state,
-        converted: convert(action.payload).from('fl-oz').to('cup').toFixed(2),
+        converted: (action.payload * 0.39525).toFixed(2),
+      };
+    case 'cupFlourToKg':
+      return {
+        ...state,
+        converted: (action.payload * 0.12).toFixed(2),
+      };
+
+    case 'cupSugarToLb':
+      return {
+        ...state,
+        converted: (action.payload * 0.4409).toFixed(2),
+      };
+
+    case 'cupRiceToGr':
+      return {
+        ...state,
+        converted: (action.payload * 195).toFixed(2),
+      };
+    case 'grToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload / 200).toFixed(2),
+      };
+    case 'kgToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload * 500).toFixed(2),
+      };
+    case 'grToCupRice':
+      return {
+        ...state,
+        converted: (action.payload / 195).toFixed(2),
+      };
+    case 'kgToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 5.1282).toFixed(2),
+      };
+    case 'grToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload / 90).toFixed(2),
+      };
+    case 'kgToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload * 11.1111).toFixed(2),
+      };
+    case 'grToCupOats':
+      return {
+        ...state,
+        converted: (action.payload / 90).toFixed(2),
+      };
+    case 'kgToCupOats':
+      return {
+        ...state,
+        converted: (action.payload * 11.1111).toFixed(2),
+      };
+    case 'mgToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload / 200000).toFixed(2),
+      };
+    case 'µgToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload / 200000000).toFixed(2),
+      };
+    case 'tonToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload * 500000).toFixed(2),
+      };
+    case 'lbToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload * 1133.98).toFixed(2),
+      };
+    case 'ozToCupSugar':
+      return {
+        ...state,
+        converted: (action.payload * 14.3).toFixed(2),
+      };
+    case 'mgToCupRice':
+      return {
+        ...state,
+        converted: (action.payload / 180000).toFixed(2),
+      };
+    case 'µgToCupRice':
+      return {
+        ...state,
+        converted: (action.payload / 180000000).toFixed(2),
+      };
+    case 'tonToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 5555.56).toFixed(2),
+      };
+    case 'lbToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 12658.39).toFixed(2),
+      };
+    case 'ozToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 160.77).toFixed(2),
       };
 
     case 'mgToCupFlour':
       return {
         ...state,
-        converted: convert(action.payload).from('mg').to('cup').toFixed(2),
+        converted: (action.payload / 120000).toFixed(2),
       };
-
     case 'µgToCupFlour':
       return {
         ...state,
-        converted: convert(action.payload).from('µg').to('cup').toFixed(2),
+        converted: (action.payload / 120000000).toFixed(2),
       };
-
     case 'tonToCupFlour':
       return {
         ...state,
-        converted: convert(action.payload).from('ton').to('cup').toFixed(2),
+        converted: (action.payload * 4166.67).toFixed(2),
       };
-
     case 'lbToCupFlour':
       return {
         ...state,
-        converted: convert(action.payload).from('lb').to('cup').toFixed(2),
+        converted: (action.payload * 9488.32).toFixed(2),
       };
-
     case 'ozToCupFlour':
       return {
         ...state,
-        converted: convert(action.payload).from('fl-oz').to('cup').toFixed(2),
+        converted: (action.payload * 120.1).toFixed(2),
       };
 
     case 'mgToCupOats':
       return {
         ...state,
-        converted: convert(action.payload).from('mg').to('cup').toFixed(2),
+        converted: (action.payload / 110000).toFixed(2),
       };
-
     case 'µgToCupOats':
       return {
         ...state,
-        converted: convert(action.payload).from('µg').to('cup').toFixed(2),
+        converted: (action.payload / 110000000).toFixed(2),
       };
-
     case 'tonToCupOats':
       return {
         ...state,
-        converted: convert(action.payload).from('ton').to('cup').toFixed(2),
+        converted: (action.payload * 4545.45).toFixed(2),
       };
-
     case 'lbToCupOats':
       return {
         ...state,
-        converted: convert(action.payload).from('lb').to('cup').toFixed(2),
+        converted: (action.payload * 10363.64).toFixed(2),
       };
-
     case 'ozToCupOats':
       return {
         ...state,
-        converted: convert(action.payload).from('fl-oz').to('cup').toFixed(2),
+        converted: (action.payload * 131.82).toFixed(2),
+      };
+    case 'mgToCupRice':
+      return {
+        ...state,
+        converted: (action.payload / 180000).toFixed(2),
+      };
+    case 'µgToCupRice':
+      return {
+        ...state,
+        converted: (action.payload / 180000000).toFixed(2),
+      };
+    case 'tonToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 5555.56).toFixed(2),
+      };
+    case 'lbToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 12658.39).toFixed(2),
+      };
+    case 'ozToCupRice':
+      return {
+        ...state,
+        converted: (action.payload * 160.77).toFixed(2),
+      };
+
+    case 'mgToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload / 120000).toFixed(2),
+      };
+    case 'µgToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload / 120000000).toFixed(2),
+      };
+    case 'tonToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload * 4166.67).toFixed(2),
+      };
+    case 'lbToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload * 9488.32).toFixed(2),
+      };
+    case 'ozToCupFlour':
+      return {
+        ...state,
+        converted: (action.payload * 120.1).toFixed(2),
+      };
+
+    case 'mgToCupOats':
+      return {
+        ...state,
+        converted: (action.payload / 110000).toFixed(2),
+      };
+    case 'µgToCupOats':
+      return {
+        ...state,
+        converted: (action.payload / 110000000).toFixed(2),
+      };
+    case 'tonToCupOats':
+      return {
+        ...state,
+        converted: (action.payload * 4545.45).toFixed(2),
+      };
+    case 'lbToCupOats':
+      return {
+        ...state,
+        converted: (action.payload * 10363.64).toFixed(2),
+      };
+    case 'ozToCupOats':
+      return {
+        ...state,
+        converted: (action.payload * 131.82).toFixed(2),
       };
 
     default:
