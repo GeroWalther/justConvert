83f24e434ceea2611a0bfb13aa326bcafcae3fd6
GeroWalther <gero.walther@gmail.com>
2023-09-02 15:53:51 +0200

    nav fixed and SubModal designed 2

--- screens/SubsriptionModalScreen.js
+++ screens/SubsriptionModalScreen.js
@@ -17,27 +17,29 @@ const SubsriptionModalScreen = ({ navigation }) => {
   return (
     <ScrollView className='bg-slate-700'>
       <CloseBtn onClose={() => navigation.goBack()} />
-      <View className='items-center justify-between px-3 py-4'>
-        <Text className='text-4xl text-center text-slate-200 mb-10'>
-          Subscribe and get full access to all features
+      <View className='items-center'>
+        <View className='items-center justify-between px-3 py-4'>
+          <Text className='text-4xl text-center text-slate-200 mb-10'>
+            Subscribe and get full access to all features
+          </Text>
+          <View className='bg-slate-500 px-7 py-5 mb-8 rounded-lg items-center'>
+            <Text className='text-base text-slate-200 p-3 uppercase'>
+              monthly $2.99
+            </Text>
+            <ActionBtn onPress={() => {}}>Subscribe & Pay</ActionBtn>
+          </View>
+          <View className='bg-slate-500 px-7 py-5 mb-5 rounded-lg items-center'>
+            <Text className='text-base text-slate-200 p-3 uppercase'>
+              yearly $8.99
+            </Text>
+            <ActionBtn onPress={() => {}}>Subscribe & Pay</ActionBtn>
+          </View>
+        </View>
+        <Text className='text-sm text-slate-400 p-3 fixed'>
+          * By clicking 'Subscribe & Pay' you agree to the terms our of
+          conditions and subscribe to a monthly/yearly subcription
         </Text>
-        <View className='bg-slate-500 px-7 py-5 mb-8 rounded-lg items-center'>
-          <Text className='text-base text-slate-200 p-3 uppercase'>
-            monthly $2.99
-          </Text>
-          <ActionBtn onPress={() => {}}>Subscribe & Pay</ActionBtn>
-        </View>
-        <View className='bg-slate-500 px-7 py-5 mb-5 rounded-lg items-center'>
-          <Text className='text-base text-slate-200 p-3 uppercase'>
-            yearly $8.99
-          </Text>
-          <ActionBtn onPress={() => {}}>Subscribe & Pay</ActionBtn>
-        </View>
       </View>
-      <Text className='text-sm text-slate-400 p-3'>
-        * By clicking 'Subscribe & Pay' you agree to the terms our of conditions
-        and subscribe to a monthly/yearly subcription
-      </Text>
     </ScrollView>
   );
 };
